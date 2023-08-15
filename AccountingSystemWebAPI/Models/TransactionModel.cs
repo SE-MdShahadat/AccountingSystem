4b6ad2113227dd153f36d7c1366aced1dcdd2884
SE-MdShahadat <se.mdshahadat@gmail.com>
2023-08-15 21:13:31 +0600

    Add project files.

---
+++ AccountingSystemWebAPI/Models/TransactionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AccountingSystemWebAPI.Models
+{
+    public class TransactionModel
+    {
+        public int TransactionId { get; set; }
+        public DateTime Date { get; set; }
+        public decimal Amount { get; set; }
+        public string Description { get; set; }
+
+    }
+}
