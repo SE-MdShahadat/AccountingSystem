98c7086b192d5c042974eafddc65cc6e9bdf2193
SE-MdShahadat <se.mdshahadat@gmail.com>
2023-08-15 21:17:31 +0600

    Transaction  Model and Api Controller Added

---
+++ AccountingSystemWebAPI/Controllers/TransactionsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Transactions;
+
+namespace AccountingSystemWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactions()
+        {
+            var transactions = await _context.Transactions.ToListAsync();
+            return Ok(transactions);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Transaction>> GetTransaction(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(transaction);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Transaction>> AddTransaction(Transaction transaction)
+        {
+            _context.Transactions.Add(transaction);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction(nameof(GetTransaction), new { id = transaction.TransactionId }, transaction);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateTransaction(int id, Transaction transaction)
+        {
+            if (id != transaction.TransactionId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(transaction).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteTransaction(int id)
+        {
+            var transaction = await _context.Transactions.FindAsync(id);
+
+            if (transaction == null)
+            {
+                return NotFound();
+            }
+
+            _context.Transactions.Remove(transaction);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
--- AccountingSystemWebAPI/Program.cs
+++ AccountingSystemWebAPI/Program.cs
@@ -6,6 +6,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+//builder.Services.AddControllers();
 
 var app = builder.Build();
 
